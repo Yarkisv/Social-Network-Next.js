eb85af3fc44f93809a9a11fbacafa877cf27ae0a
Hann_zshish <seldmq@gmail.com>
2025-06-07 00:21:33 +0300

    branch test

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -76,7 +76,7 @@ export default function RegisterPage() {
             onChange={handleChange}
             required
             autoComplete="off"
-            className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full px-3 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
 
           <input
